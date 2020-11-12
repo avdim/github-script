f22962cb470d0921dd27737063e8bf7ce8b64771
avdim <avdim@mail.ru>
2020-11-12 21:13:41 +0300

    prepare migration test

--- __test__/migrate.test.ts
+++ __test__/migrate.test.ts
@@ -1,9 +1,15 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
 import * as fs from 'fs'
+// const migrate = require('npm-migrate')
+const moduleName = 'test-package'
+const from = 'http://localhost:55551'
+const to = 'http://localhost:55552'
 
 describe('migrate', () => {
   test('test1', async () => {
+    // console.log("migrate fun: ", migrate)
+    // migrate(moduleName, from, to, {debug: false})
     expect('a').toEqual('a')
   })
 })
