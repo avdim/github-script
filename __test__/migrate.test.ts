42e486a01ca3abbd9a7f6f5963dd26cd1f05e50c
avdim <avdim@mail.ru>
2020-11-12 18:42:54 +0300

    integration_test.sh

---
+++ __test__/migrate.test.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
+import * as fs from 'fs'
+
+describe('migrate', () => {
+  test('test1', async () => {
+    expect('a').toEqual('a')
+  })
+})
---
+++ integration_test.sh
@@ -0,0 +1,38 @@
+function clear {
+  echo "clear"
+  docker kill $(docker ps -q)
+  docker rm $(docker ps -a -q)
+}
+
+#docker run -it --rm --name verdaccio -p 4873:4873 verdaccio/verdaccio
+
+#docker run --name verdaccio -p 4873:4873 verdaccio/verdaccio
+#docker run --name verdaccio -p 4874:4873 verdaccio/verdaccio
+
+clear
+
+docker-compose --file npm-registries.yml up -d
+sleep 4
+echo "start"
+#curl http://localhost:55551
+#curl http://localhost:55552
+
+#npm login --registry=http://localhost:55551 --scope=@tutu
+npm-cli-login -u Username -p Password -e test@example.com -r http://localhost:55551
+cd /Users/dim/Desktop/github/tutu/js-npm-migrate
+npm publish
+cd -
+npm-cli-login -u Username -p Password -e test@example.com -r http://localhost:55552
+
+echo "check: http://localhost:55551"
+read check1
+jest --testNamePattern=migrate
+#npm test
+
+echo "check: http://localhost:55552"
+read check2
+
+#todo make assert
+echo "test complete success"
+
+clear
