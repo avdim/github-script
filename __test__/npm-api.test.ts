d6e2e987964548dd9b366f53d7aa242135a641b2
avdim <avdim@mail.ru>
2020-11-24 23:19:05 +0300

    addUser

--- __test__/npm-api.test.ts
+++ __test__/npm-api.test.ts
@@ -11,7 +11,8 @@ const TGZ_PATH = path.resolve(__dirname, "resources", "test-package-1.0.1.tgz");
 const AUTH = {
   username: "Username",
   password: "Password",
-  email: "test@example.com"
+  email: "test@example.com",
+  // alwaysAuth: true,
 };
 
 describe('npm-api', () => {
@@ -33,6 +34,7 @@ describe('npm-api', () => {
     jest.setTimeout(3 * 60_000);
     //https://github.com/npm/npm-registry-client
     //https://github.com/postmanlabs/npm-cli-login/blob/master/lib/login.js#L51
+    await addUser(REGISTRY, AUTH)
     await publishAsync(REGISTRY, TGZ_PATH, MODULE_NAME, AUTH)
     expect(await getModuleInfo(REGISTRY, MODULE_NAME, "1.0.1", AUTH) != undefined).toEqual(true)
   })
@@ -41,6 +43,26 @@ describe('npm-api', () => {
 
 function testSkip(name: string, lambda: any) {
 
+}
+
+async function addUser(registryUrl: string, auth: any): Promise<unknown> {
+  return await new Promise((resolve: (value?: unknown) => void, reject: (reason?: any) => void) => {
+    const regClient = new RegClient();
+    regClient.adduser(
+      registryUrl,
+      {
+        auth: auth
+      },
+      (err: any, data: any) => {
+        if (err) {
+          console.log("err: ", err)
+          reject(err)
+        } else {
+          resolve(data)
+        }
+      }
+    )
+  });
 }
 
 async function publishAsync(registryUrl: string, tgzPath: string, moduleName: string, auth: any): Promise<unknown> {
@@ -52,7 +74,7 @@ async function publishAsync(registryUrl: string, tgzPath: string, moduleName: st
         body: fs.createReadStream(tgzPath),
         metadata: {
           name: moduleName,//todo определять автоматически
-          version: "1.0.1"
+          version: "1.0.1"//todo определять автоматически
         },
         auth: auth
       },
--- test-npm-api.sh
+++ test-npm-api.sh
@@ -12,8 +12,6 @@ docker-compose --file npm-registries.yml up -d
 sleep 2
 #echo "start"
 
-node_modules/.bin/npm-cli-login -u Username -p Password -e test@example.com -r http://localhost:55552
-
 node_modules/.bin/jest --testNamePattern=npm-api && echo "upload test success" # && clear
 
 #clear #todo clear if fails
