46a0bc7155febc2f2ce937b2a3628d2fb072b6da
avdim <avdim@mail.ru>
2020-11-26 17:42:53 +0300

    container1 to container2

--- __test__/npm-api.test.ts
+++ __test__/npm-api.test.ts
@@ -31,17 +31,23 @@ describe('npm-api', () => {
 
   test('upload type script', async () => {
     jest.setTimeout(3 * 60_000);
-    const {startedContainer, registryUrl} = await createEmptyNpmRegistry();
+    const {startedContainer: container1, registryUrl: registry1} = await createEmptyNpmRegistry();
+    const {startedContainer: container2, registryUrl: registry2} = await createEmptyNpmRegistry();
     try {
       //https://github.com/npm/npm-registry-client
       //https://github.com/postmanlabs/npm-cli-login/blob/master/lib/login.js#L51
-      await addUser(registryUrl, AUTH)
-      await publishAsync(registryUrl, TGZ_PATH, MODULE_NAME, AUTH)
-      const moduleInfo:any = await getModuleInfo(registryUrl, MODULE_NAME, MODULE_VERSION, AUTH);
-      expect(moduleInfo != undefined).toEqual(true)
-      const tmpTgz = await downloadTarball(registryUrl, MODULE_NAME, MODULE_VERSION, AUTH);
+      await addUser(registry1, AUTH)
+      await publishAsync(registry1, TGZ_PATH, MODULE_NAME, AUTH)
+      const moduleInfo1:any = await getModuleInfo(registry1, MODULE_NAME, MODULE_VERSION, AUTH);
+      expect(moduleInfo1 != undefined).toEqual(true)
+      const tmpTgz = await downloadTarball(registry1, MODULE_NAME, MODULE_VERSION, AUTH);
+      await addUser(registry2, AUTH)
+      await publishAsync(registry2, tmpTgz, MODULE_NAME, AUTH)
+      const moduleInfo2:any = await getModuleInfo(registry2, MODULE_NAME, MODULE_VERSION, AUTH);
+      expect(moduleInfo2 != undefined).toEqual(true)
     } finally {
-      startedContainer.stop()
+      container1.stop()
+      container2.stop()
     }
   })
 
